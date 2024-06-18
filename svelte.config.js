e200367ecfd816cdcfbd0cdfbb2899dd8e384fc1
Noshad Boksh <noshadb2810@gmail.com>
2024-06-18 01:02:36 +0100

    Removed target

--- svelte.config.js
+++ svelte.config.js
@@ -2,11 +2,13 @@ import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// Specify the target element where the Svelte app will be mounted
-		target: '#svelte',
-		adapter: adapter(),
-	},
+  kit: {
+    // The target option has been removed in newer versions of SvelteKit.
+    // You can safely remove this line.
+    // target: '#svelte',
+
+    adapter: adapter(),
+  },
 };
 
 export default config;
